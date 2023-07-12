d3e518a9d244852c86fd7f6e3f9d6e788176c2f2
Anthony <anthony@defenseunicorns.com>
2023-07-12 17:03:31 -0600

    Update e2e

--- test/e2e/e2e_basic_smoke_test.go
+++ test/e2e/e2e_basic_smoke_test.go
@@ -35,10 +35,6 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status deployment/gitlab-gitlab-webservice-default -n gitlab --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 
-		// Ensure that GitLab is available outside of the cluster.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done"`)
-		require.NoError(t, err, output)
-
 		// Ensure that the services do not accept discontinued TLS versions. If they reject TLSv1.1 it is assumed that they also reject anything below TLSv1.1.
 		// Ensure that GitLab does not accept TLSv1.1
 		output, err = platform.RunSSHCommandAsSudo(`sslscan gitlab.bigbang.dev | grep "TLSv1.1" | grep "disabled"`)
